4792b8fcd27106dfd98da80d688c46f34ecee9a1
LucasAdrian0 <https://github.com/LucasAdrian0>
2024-10-26 05:19:42 -0300

    filtrar contatos por texto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { ContainerComponent } from './componentes/container/container.component'
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
 import { ContatoComponent } from './componentes/contato/contato.component';
+import { FormsModule } from '@angular/forms';
+
 
 //interface que representa um contato
 interface Contato {
@@ -24,12 +26,34 @@ import agenda from './agenda.json'
     ContainerComponent, 
     CabecalhoComponent, 
     SeparadorComponent,
-    ContatoComponent
+    ContatoComponent,
+    FormsModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
-  contatos: Contato[] = agenda
+  contatos: Contato[] = agenda;
+
+  //guardar valor
+  filtroPorTexto: string = ''
+
+  //metodo para filtrar contatos por texto
+  filtrarContatosPorTexto(): Contato[] {
+    if (this.filtroPorTexto) {
+      return this.contatos
+    }
+    return this.contatos.filter(contato =>{
+      return contato.nome.toLocaleLowerCase().includes(this.filtroPorTexto.toLocaleLowerCase())
+    })
+
+  }
+
+  //metodo para filtrar contatos por letras inicial
+  filtrarContatosPorLetraInicial(letras: string) : Contato[] {
+    return this.filtrarContatosPorTexto().filter( contato => {
+      return contato.nome.toLocaleLowerCase().startsWith(letras)
+    }) 
+  }
 }
