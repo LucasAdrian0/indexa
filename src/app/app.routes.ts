7a5cc76c5530ac8c2664688519dc23eb9e780381
LucasAdrian0 <https://github.com/LucasAdrian0>
2024-10-27 19:49:29 -0300

    inicializado controle de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { FormularioContatoComponent } from './paginas/formulario-contato/formulario-contato.component';
+import { ListaContatosComponent } from './paginas/lista-contatos/lista-contatos.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+//aqui deve constar os caminhos que o angular deve seguir
+{    path: 'formulario' , component: FormularioContatoComponent},
+{path: 'lista-contatos', component: ListaContatosComponent}
+];
