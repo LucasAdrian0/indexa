895780f473e0654bedfaaf92979c94645d8fe28f
LucasAdrian0 <https://github.com/LucasAdrian0>
2024-10-27 00:50:37 -0300

    declarando propriedades que representa o formulario, constructo, formgroup,formcontrol

--- src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { SeparadorComponent } from '../../componentes/separador/separador.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
@@ -10,11 +11,28 @@ import { SeparadorComponent } from '../../componentes/separador/separador.compon
   imports: [
     CommonModule,
     ContainerComponent,
-    SeparadorComponent
+    SeparadorComponent,
+    ReactiveFormsModule //utiliza este importe para trabalhar com formulario
   ],
   templateUrl: './formulario-contato.component.html',
   styleUrl: './formulario-contato.component.css'
 })
 export class FormularioContatoComponent {
+  //declarar uma propriedade que vai representar o formulario
+  contatoForm!/*! representa operador não nula */: FormGroup;//formgroup é uma ferramenta para trabalhar com formulário, vai apresentar erro pq ela não foi inicializada, deve inicializar ela dentro do construtor no tipescript
+
+  //construtor da classe contatoForm
+  constructor(){
+    this.contatoForm = new FormGroup({//contatoForm recebe um grupo de formulario
+      //aqui devo escrever todos os campos do formulario
+      nome: new FormControl(''), //formcontrol fala que nome recebe campo de controle, dentro do parenteses pode passar um valor inicialou não, neste caso ficou como string vazia
+      telefone: new FormControl('') ,
+      email: new FormControl('') ,
+      aniversario: new FormControl('') ,
+      redes: new FormControl(''),
+      observacoes: new FormControl('') 
+
+    })
+  }
 
 }
