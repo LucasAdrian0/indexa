fc93b57bd6600767571ba10b8026a9d42fc8bdc7
LucasAdrian0 <https://github.com/LucasAdrian0>
2024-10-27 20:21:12 -0300

    configurado rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,13 @@ import { ListaContatosComponent } from './paginas/lista-contatos/lista-contatos.
 
 export const routes: Routes = [
 //aqui deve constar os caminhos que o angular deve seguir
-{    path: 'formulario' , component: FormularioContatoComponent},
-{path: 'lista-contatos', component: ListaContatosComponent}
+{path: 'formulario' , component: FormularioContatoComponent},
+{path: 'lista-contatos', component: ListaContatosComponent},
+{
+    path: '', 
+    redirectTo:'/lista-contatos',
+    pathMatch:'full'//se o roteador for 4200 redireciona para lista de contatos- existe dois tipos:
+    //'prefix': Indica que a rota deve ser correspondida se a URL começa com o padrão definido na rota.
+    //'full': Indica que a rota deve ser correspondida somente se a URL completa corresponder ao padrão definido na rota.
+}
 ];
